77bd9a56ea8c00eafa3b4af20854e40ebc93044b
Nicolas Aguilera García <niagar25@gmail.com>
2021-01-02 18:25:11 -0500

    Cuarto Commit: --> Finalizacion pagina de Contacto. --> Finalizacion pagina de Succes. --> Pendiente realizar diseño Responsive.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const nodemailer = require('nodemailer');
 
 app.use(express.urlencoded({extended: false}))
 
@@ -23,14 +24,49 @@ app.listen(process.env.PORT || app.get('puerto'), () => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Configuracion de rutas
+// ----> Home
 app.get('/', (req, res) => {
-    res.render('landPage.html');
+  res.render('landPage.html');
 });
-
+// ----> Contacto
 app.get('/contacto', (req, res) => {
-    res.render('contactPage.html');
+  res.render('contactPage.html');
 });
-
+// ----> Info
 app.get('/info', (req, res) => {
-    res.render('infoPage.html');
+  res.render('infoPage.html');
+});
+// ----> Enviar Mail
+app.post('/send-email', async (req, res) => {
+  const { nombre, correo, mensaje } = req.body;
+
+  contenidoHTML = ` 
+    <h1>Datos de usuarios</h1>
+    <ul>
+      <li>Nombre: ${nombre}</li>
+      <li>Correo: ${correo}</li>
+    </ul>
+    <p>${mensaje}</p>
+  `;
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'golden.rau84@ethereal.email',
+        pass: 'Mvn4GgpMwjcPus8Zsp'
+    },
+    tls: {
+      rejectUnauthorized: false,
+    }
+  });
+
+  const info = await transporter.sendMail({
+    from: 'Contacto --- Proyecto Planetas',
+    to: 'nicholson2002@outlook.es',
+    subject: 'Contacto del sitio web',
+    html: contenidoHTML,
+  });
+
+  res.render('successPage.html');
 });
